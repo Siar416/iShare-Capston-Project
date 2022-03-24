0488296f9c59ce879addd907608bdcfebeafc1d8
Siar Wahidi <siar.wahidi@gmail.com>
2022-03-23 21:44:46 -0400

    added boiler plate for server index. Created env file, connected to mongoose db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import mongoose from "mongoose";
+import doteven from "dotenv";
+
+const app = express();
+
+import cors from "cors";
+
+app.use(bodyParser.json({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(doteven.config().parsed.CONNECTION_DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err));
