dec03c8588d293c966754b5bd5771d49fa660bd0
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-03 00:01:02 -0400

    deploy testing

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res) => {
-  // res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // res.sendFile("index.html");
 });
 
 mongoose
