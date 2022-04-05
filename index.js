13e52dfb81b0a59ad081b920f41aff9fe9950014
Siar <83523212+Siar416@users.noreply.github.com>
2022-04-05 12:48:18 -0400

    Update index.js
    removed comment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
-// 👇️ "/home/john/Desktop/javascript"
+
 const __dirname = path.dirname(__filename);
 
 const app = express();
